94fec6ef8f79c8687d8446a9064f3bd4ac30df58
sofacles <sofacles@yahoo.com>
2024-10-05 08:14:31 -0700

    more cleanup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,10 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Component from './Component';
 
-import "../styles/main.scss";
-
 const App = () => (
-  <BrowserRouter>
-    <div className="app">
-      <div className="app-body">
-        <div className="main-content">
-          <Routes>
-            <Route path="/" element={<Component />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
-  </BrowserRouter>
+  <div className="app">
+    <Component />
+  </div>
 );
 
 export default App;
